be6fdb1dae98d10ad258f32b0168651ae7282e2a
omnimo86 <cma.cona.21@gmail.com>
2025-03-12 22:30:18 -0300

    creacion de funcion agregar amigo

--- app.js
+++ app.js
@@ -2,7 +2,24 @@
 let amigos = [];
 
 // crear funcion para agregar amigos
+function agregarAmigo() {
+    let inputAmigo = document.getElementById('amigo').value;
 
+    // Validación
+    if (inputAmigo === '') {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    // Valor que no se repita
+    if (amigos.includes(inputAmigo)) {
+        alert(`El nombre ${inputAmigo} ya está en la lista`);
+        return;
+    }
+
+    amigos.push(inputAmigo); // Agrega nombre al array amigos
+    document.getElementById('amigo').value = ""; // Limpia el input
+    actualizarLista();   
+}
 
 // crea funcion para crear lista de amigos
 
